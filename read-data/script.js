0a205176d073e41c05968670361c7e829c62de86
johannes <jmuljana@gmail.com>
2017-05-16 08:53:43 +1000

    reading and enumerating data

--- read-data/script.js
+++ read-data/script.js
@@ -2,43 +2,40 @@
 var app = {
   userid: '',
   useremail: '',
-  useraccountstatus:'',
-  view:''
+  useraccountstatus: '',
+  view: ''
 }
 //firebase observer for user state
 
 
-function formController(){
-  if(app.userid){
+function formController() {
+  if (app.userid) {
     //hide both forms
     app.signin.classList.add('hide');
     app.signup.classList.add('hide');
-  }
-  else{
+  } else {
     app.signin.classList.remove('hide');
     app.signup.classList.add('hide');
   }
 }
 
-function toggleForms(){
+function toggleForms() {
   var forms = document.getElementsByClassName('auth-ui');
-  for(i=0;i<forms.length;i++){
-    if(forms[i].classList.contains('hide')){
+  for (i = 0; i < forms.length; i++) {
+    if (forms[i].classList.contains('hide')) {
       forms[i].classList.remove('hide');
-    }
-    else{
+    } else {
       forms[i].classList.add('hide');
     }
   }
 }
 
-function menuController(){
+function menuController() {
   //if user is logged in
-  if(app.userid){
+  if (app.userid) {
     //show logout link
     document.getElementById('logoutbtn').classList.remove('hide');
-  }
-  else{
+  } else {
     //hide logout link
     document.getElementById('logoutbtn').classList.add('hide');
   }
@@ -51,33 +48,33 @@ function onWindowLoad() {
   bindUI();
 }
 
-function bindUI(){
+function bindUI() {
   //get a reference to the sign in form and store it in global app object
   //for easy access
   app.signin = document.getElementById('signin');
   //listen to submit event from the login form
   app.signin.addEventListener('submit', signUserIn);
-  
+
   //get a reference to the sign up form
   app.signup = document.getElementById('signup');
   //listen to submti event from the signup form
-  app.signup.addEventListener('submit',signUserUp);
-  
+  app.signup.addEventListener('submit', signUserUp);
+
   //get a reference to the logout button
   app.logout = document.getElementById('logoutbtn');
-  app.logout.addEventListener('click',logUserOut);
+  app.logout.addEventListener('click', logUserOut);
 
   //add a listener for the task form
-  let taskform = document.getElementById('task-form');
+  var taskform = document.getElementById('task-form');
   taskform.addEventListener('submit', addTask);
-  
+
   //add listener to toggle forms
-  let formtoggles = document.getElementsByClassName('auth-ui');
-  for(t=0;t<formtoggles.length;t++){
+  var formtoggles = document.getElementsByClassName('auth-ui');
+  for (t = 0; t < formtoggles.length; t++) {
     link = formtoggles[t].getElementsByClassName('form-toggle');
-    link[0].addEventListener('click',toggleForms);
+    link[0].addEventListener('click', toggleForms);
   }
-  
+
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
@@ -97,27 +94,59 @@ function bindUI(){
   });
 }
 
-function signUserUp(event){
+function signUserUp(event) {
   //stop the form from refreshing page by cancelling the event
   event.preventDefault();
   //get value of email from sign up form
-  let email = document.getElementById('signup-email').value;
+  var email = document.getElementById('signup-email').value;
   //get the password
-  let password = document.getElementById('signup-password').value;
+  var password = document.getElementById('signup-password').value;
   //username
+  var username = document.getElementById('signup-name').value;
+  //sign user up with email and password
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      //reset the form
+      event.target.reset();
+      //get the id of the user
+      var userid = firebase.auth().currentUser.uid;
+      //write username to database using user.id as key, under users branch
+      var ref = 'users/' + userid;
+      var obj = {
+        name: username
+      };
+      writeData(ref, obj);
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
 }
 
 function signUserIn(event) {
   //stop the form from refreshing page by cancelling the event
   event.preventDefault();
   //get value of email from sign in form
-  let email = document.getElementById('signin-email').value;
+  var email = document.getElementById('signin-email').value;
   //get value of password from sign in form
-  let password = document.getElementById('signin-password').value;
+  var password = document.getElementById('signin-password').value;
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function () {
       //empty the form
       document.getElementById('signin-form').reset();
+      //get user id
+      var userid = firebase.auth().currentUser.uid;
+      //get user name
+      firebase.database().ref('users/' + userid).once('value')
+        .then(function (snapshot) {
+          var username = snapshot.val().name;
+          //greet user by printing username in notifications area
+          notifyUser("Hello " + username);
+        });
+      //get tasks
+      readTasks(userid);
     })
     .catch(function (error) {
       notifyUser(error.message);
@@ -126,7 +155,7 @@ function signUserIn(event) {
 
 function logUserOut() {
   firebase.auth().signOut().then(function () {
-    
+    notifyUser('');
   }).catch(function (error) {
     notifyUser(error.message);
   });
@@ -134,23 +163,23 @@ function logUserOut() {
 
 
 function notifyUser(msg) {
-  document.getElementById('message').val = msg;
+  document.getElementById('message').innerHTML = msg;
 }
 
 function addTask(event) {
   //stop form from refreshing page by cancelling the event default
   event.preventDefault();
   //get the value of the input
-  let task = document.getElementById('task-input').value;
+  var task = document.getElementById('task-input').value;
   //empty the form
   event.target.reset();
   //create id for task using timestamp
-  let taskid = new Date().getTime();
+  var taskid = new Date().getTime();
   //write data to firebase using firebase.database object
   //write tasks to "tasks" branch, use userid as key and taskid as key
   //for each task
-  let dataref = 'tasks/' + app.userid + '/' + taskid;
-  let dataobj = {
+  var dataref = 'tasks/' + app.userid + '/' + taskid;
+  var dataobj = {
     taskname: task,
     status: 0
   };
@@ -164,15 +193,30 @@ function writeData(ref, obj) {
     });
 }
 
-function readData(id) {
+function readTasks(id) {
+  //create reference to the branch with tasks
   firebase.database().ref('tasks/' + id).once('value')
     .then(function (snapshot) {
-      let tasks = snapshot.val();
-      let taskcount = Object.keys(tasks).length;
-      console.log(Object.keys(tasks));
+      var tasks = snapshot.val();
+      var taskcount = Object.keys(tasks).length;
+      renderTasks(tasks);
+    })
+    .catch(function (error) {
+      console.log(error.message);
     });
 }
 
-function renderData(dataobj) {
-
+function renderTasks(dataobj) {
+  //get the length of tasks
+  var count = Object.keys(dataobj).length;
+  var keys = Object.keys(dataobj);
+  var vals = Object.values(dataobj);
+  //loop through the tasks
+  var i=0;
+  for(i=0;i<count;i++){
+    var task = keys[i];
+    console.log(task);
+    console.log(vals[i].taskname);
+    //console.log(dataobj.task.taskname);
+  }
 }
